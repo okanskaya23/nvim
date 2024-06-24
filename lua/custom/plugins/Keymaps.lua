c97a221805074b18a5d49ab2edc78636c7bb9772
Okan Sarp Kaya <okanskaya@gmail.com>
2024-06-24 22:48:28 +0300

    Add fugetive keymaps

--- lua/custom/plugins/Keymaps.lua
+++ lua/custom/plugins/Keymaps.lua
@@ -1,3 +1,8 @@
 return {
+
+  -- Old Files
   vim.api.nvim_set_keymap('n', '<leader>fr', ':Telescope oldfiles<CR>', { noremap = true, silent = true, desc = 'Open Recent Files' }),
+
+  --Git
+  vim.api.nvim_set_keymap('n', '<leader>G', ':Git<CR>', { noremap = true, silent = true, desc = 'Git' }),
 }
