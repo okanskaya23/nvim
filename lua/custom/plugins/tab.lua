b224c1eedab25fbcc74a206ac251c640dcc55341
Okan Sarp Kaya <okanskaya@gmail.com>
2024-07-03 22:15:18 +0300

    Buffer line maps

--- lua/custom/plugins/tab.lua
+++ lua/custom/plugins/tab.lua
@@ -5,6 +5,16 @@ return {
     dependencies = 'nvim-tree/nvim-web-devicons',
     config = function()
       require('bufferline').setup {}
+      local map = vim.api.nvim_set_keymap
+
+      options = { noremap = true, silent = true }
+
+      map('n', '<leader>bp', '<Cmd>BufferLineTogglePin<CR>', options)
+      map('n', '<leader>bo', '<Cmd>BufferLineCloseOthers<CR>', options)
+      map('n', '<leader>br', '<Cmd>BufferLineCloseRight<CR>', options)
+      map('n', '<leader>bl', '<Cmd>BufferLineCloseLeft<CR>', options)
+      map('n', '<leader>bb', '<Cmd>BufferLinePick<CR>', options)
+      map('n', '<leader>bc', '<Cmd>BufferLinePickClose<CR>', options)
     end,
   },
 }
