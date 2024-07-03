78bc139e31cdd18caf8cdebdf6205f55b72f77df
Okan Sarp Kaya <okanskaya@gmail.com>
2024-07-03 21:41:30 +0300

    Add tabs

--- lua/custom/plugins/Keymaps.lua
+++ lua/custom/plugins/Keymaps.lua
@@ -5,13 +5,4 @@ return {
 
   --Git
   vim.api.nvim_set_keymap('n', '<leader>g', ':Git<CR>', { noremap = true, silent = true, desc = 'Git' }),
-
-  -- restore the session for the current directory
-  vim.api.nvim_set_keymap('n', '<leader>qs', [[<cmd>lua require("persistence").load()<cr>]], {}),
-
-  -- restore the last session
-  vim.api.nvim_set_keymap('n', '<leader>ql', [[<cmd>lua require("persistence").load({ last = true })<cr>]], {}),
-
-  -- stop Persistence => session won't be saved on exit
-  vim.api.nvim_set_keymap('n', '<leader>qd', [[<cmd>lua require("persistence").stop()<cr>]], {}),
 }
