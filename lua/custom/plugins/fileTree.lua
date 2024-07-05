f3fe5f45ead87f2c47a51f167b111cf8a7434159
Okan Sarp Kaya <okanskaya@gmail.com>
2024-07-05 23:59:55 +0300

    Nvim tree config

--- lua/custom/plugins/fileTree.lua
+++ lua/custom/plugins/fileTree.lua
@@ -1,5 +1,4 @@
 return {
-
   {
     'nvim-tree/nvim-tree.lua',
     version = '*',
@@ -7,16 +6,40 @@ return {
     dependencies = {
       'nvim-tree/nvim-web-devicons',
     },
-
-    vim.api.nvim_set_keymap(
-      'n',
-      '<leader>e',
-      "<cmd> lua require('nvim-tree.api').tree.toggle()<cr>",
-      { noremap = true, silent = true, desc = 'Open File Tree' }
-    ),
-
     config = function()
-      require('nvim-tree').setup {}
+      local function nvim_tree_toggle_and_find()
+        local nvim_tree = require 'nvim-tree.api'
+        local current_buf = vim.api.nvim_get_current_buf()
+        if vim.bo[current_buf].filetype == 'NvimTree' then
+          nvim_tree.tree.toggle()
+        else
+          if not nvim_tree.tree.is_visible() then
+            nvim_tree.tree.open()
+          end
+          nvim_tree.tree.find_file { open = true, focus = true }
+        end
+      end
+
+      vim.keymap.set('n', '<leader>e', nvim_tree_toggle_and_find, { noremap = true, silent = true, desc = 'Toggle NvimTree' })
+
+      require('nvim-tree').setup {
+        filters = {
+          dotfiles = false, -- Show dotfiles
+          custom = { '^.git$', '^node_modules$', '^.meta$', '^LICENSE$' }, -- Ignore files/dirs
+          --exclude = { '.gitignore' }, -- Don't ignore .gitignore
+        },
+        update_focused_file = {
+          enable = true,
+          update_root = true,
+        },
+        view = {
+          width = 30,
+        },
+        renderer = {
+          highlight_git = true,
+          highlight_opened_files = 'all',
+        },
+      }
     end,
   },
 }
