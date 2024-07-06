8cf15a56088da15ae064311817316e9576496c43
Okan Sarp Kaya <okanskaya@gmail.com>
2024-07-06 11:08:00 +0300

    Telescope maps and remove from init file

--- lua/custom/plugins/Keymaps.lua
+++ lua/custom/plugins/Keymaps.lua
@@ -1,11 +1,4 @@
 return {
-
-  -- Old Files
-  vim.api.nvim_set_keymap('n', '<leader>fr', ':Telescope oldfiles<CR>', { noremap = true, silent = true, desc = 'Open Recent Files' }),
-
-  --Git
-  vim.api.nvim_set_keymap('n', '<leader>g', ':Git<CR>', { noremap = true, silent = true, desc = 'Git' }),
-
   vim.keymap.set({ 'n', 'x' }, 'd', '"dd', { noremap = true }),
   vim.keymap.set({ 'n', 'x' }, 'D', '"dD', { noremap = true }),
   vim.keymap.set({ 'n', 'x' }, 'x', '"dx', { noremap = true }),
