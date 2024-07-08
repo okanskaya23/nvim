e4a7b2f3b63381ea2199ecec7c57ac08fe357aca
Okan Sarp Kaya <okanskaya@gmail.com>
2024-07-08 21:56:41 +0300

    Delete tokyonigth and install catputchini

--- lua/custom/plugins/theme.lua
+++ lua/custom/plugins/theme.lua
@@ -1,39 +1,57 @@
 return {
-  'folke/tokyonight.nvim',
+  'catppuccin/nvim',
   priority = 1000,
   config = function()
-    require('tokyonight').setup {
-      style = 'night',
-      light_style = 'day',
-      transparent = false,
-      terminal_colors = false,
-      styles = {
-        comments = { italic = true },
-        keywords = { italic = true },
+    require('catppuccin').setup {
+      flavour = 'auto', -- latte, frappe, macchiato, mocha
+      background = { -- :h background
+        light = 'latte',
+        dark = 'mocha',
+      },
+      transparent_background = false, -- disables setting the background color.
+      show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+      term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+      dim_inactive = {
+        enabled = false, -- dims the background color of inactive window
+        shade = 'dark',
+        percentage = 0.15, -- percentage of the shade to apply to the inactive window
+      },
+      no_italic = false, -- Force no italic
+      no_bold = false, -- Force no bold
+      no_underline = false, -- Force no underline
+      styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+        comments = { 'italic' }, -- Change the style of comments
+        conditionals = { 'italic' },
+        loops = {},
         functions = {},
+        keywords = {},
+        strings = {},
         variables = {},
-        sidebars = 'dark',
-        floats = 'dark',
+        numbers = {},
+        booleans = {},
+        properties = {},
+        types = {},
+        operators = {},
+        -- miscs = {}, -- Uncomment to turn off hard-coded styles
       },
-      day_brightness = 0.3,
-      dim_inactive = false,
-      lualine_bold = false,
-      plugins = {
-        auto = true,
+      color_overrides = {},
+      custom_highlights = {},
+      default_integrations = true,
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+        notify = false,
+        mini = {
+          enabled = true,
+          indentscope_color = '',
+        },
+        -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
       },
-      on_highlights = function(hl, c)
-        hl.NormalFloat = {
-          bg = 'NONE',
-        }
-        hl.Terminal = {
-          bg = 'NONE',
-        }
-        hl.Comment = {
-          fg = c.comment,
-          italic = false,
-        }
-      end,
     }
-    vim.cmd.colorscheme 'tokyonight-night'
+
+    -- setup must be called before loading
+    vim.cmd.colorscheme 'catppuccin'
   end,
 }
