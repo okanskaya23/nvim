23797aa3e7a23c6a7941b73fc4e68823b5a1facc
Okan Sarp Kaya <okanskaya@gmail.com>
2024-07-14 17:08:51 +0300

    add them for notify

--- lua/custom/plugins/theme.lua
+++ lua/custom/plugins/theme.lua
@@ -42,7 +42,7 @@ return {
         gitsigns = true,
         nvimtree = true,
         treesitter = true,
-        notify = false,
+        notify = true,
         mini = {
           enabled = true,
           indentscope_color = '',
